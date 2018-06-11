da1ff0703a086cc346ea6b12c0f276ef06d2c4d5
Lawrence Cherone <lawrence@cherone.co.uk>
2018-06-11 21:52:58 +0000

    refactor

--- .api/controller/api/lxd/devices/gpu.php
+++ .api/controller/api/lxd/devices/gpu.php
@@ -16,11 +16,13 @@ class Gpu extends \Base\Controller
     {
         try {
             \Lib\JWT::checkAuthThen(function ($server) use ($f3) {
+                /*
                 $f3->set('plinker', new \Plinker\Core\Client($server, [
                     'secret' => $f3->get('AUTH.secret'),
                     'database' => $f3->get('db'),
                     'lxc_path' => $f3->get('LXC.path')
                 ]));
+                */
             });
         } catch (\Exception $e) {
             $f3->response->json([
@@ -42,7 +44,7 @@ class Gpu extends \Base\Controller
         $verb = $f3->get('VERB');
         
         // plinker client
-        $client = $f3->get('plinker');
+        //$client = $f3->get('plinker');
 
         /**
          * GET /api/lxd/devices/gpu
@@ -137,7 +139,7 @@ class Gpu extends \Base\Controller
         $verb = $f3->get('VERB');
         
         // plinker client
-        $client = $f3->get('plinker');
+        //$client = $f3->get('plinker');
         
         /**
          * GET /api/lxd/devices/gpu/@id
--- .api/controller/api/lxd/devices/infiniband.php
+++ .api/controller/api/lxd/devices/infiniband.php
@@ -16,11 +16,13 @@ class Infiniband extends \Base\Controller
     {
         try {
             \Lib\JWT::checkAuthThen(function ($server) use ($f3) {
+                /*
                 $f3->set('plinker', new \Plinker\Core\Client($server, [
                     'secret' => $f3->get('AUTH.secret'),
                     'database' => $f3->get('db'),
                     'lxc_path' => $f3->get('LXC.path')
                 ]));
+                */
             });
         } catch (\Exception $e) {
             $f3->response->json([
@@ -42,7 +44,7 @@ class Infiniband extends \Base\Controller
         $verb = $f3->get('VERB');
         
         // plinker client
-        $client = $f3->get('plinker');
+        // $client = $f3->get('plinker');
 
         /**
          * GET /api/lxd/devices/infiniband
@@ -149,7 +151,7 @@ class Infiniband extends \Base\Controller
         $verb = $f3->get('VERB');
         
         // plinker client
-        $client = $f3->get('plinker');
+        // $client = $f3->get('plinker');
         
         /**
          * GET /api/lxd/devices/infiniband/@id
