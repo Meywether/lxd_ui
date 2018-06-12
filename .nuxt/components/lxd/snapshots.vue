381831d786e375ef58364785fc2741551a40c67e
Lawrence Cherone <lawrence@cherone.co.uk>
2018-06-12 18:31:12 +0000

    .

--- .nuxt/components/lxd/snapshots.vue
+++ .nuxt/components/lxd/snapshots.vue
@@ -7,27 +7,33 @@
       <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
       <template slot="items" slot-scope="props">
         <tr>
-          <td>{{ props.item.name }}</td>
+          <td>
+            <v-form ref="form" v-model="valid">
+              <v-edit-dialog :return-value.sync="props.item.oldNameNoContainer" lazy lazy-validation>
+                {{ props.item.oldNameNoContainer }}
+                <v-text-field
+                  slot="input"
+                  v-model="props.item.oldNameNoContainer"
+                  label="Edit"
+                  single-line
+                  :rules="nameRule"
+                  @keyup.enter="actionSnapshot('rename', props.item)"
+                ></v-text-field>
+              </v-edit-dialog>
+            </v-form>
+          </td>
           <td>{{ new Date(props.item.created_at).toLocaleString() }}</td>
           <td>
-            <v-tooltip left>
-              <v-btn slot="activator" icon class="mx-0" style="float:right" @click.stop="deleteSnapshot(props.item)">
-                <v-icon color="pink">delete</v-icon>
+            <v-menu offset-y left style="float:right" class="mr-3">
+              <v-btn icon class="mx-0" slot="activator">
+                <v-icon color="blue-grey lighten-3">view_headline</v-icon>
               </v-btn>
-              <span>Delete</span>
-            </v-tooltip>
-            <v-tooltip left>
-              <v-btn slot="activator" icon class="mx-0" style="float:right" @click.stop="restoreSnapshot(props.item)">
-                <v-icon color="blue">restore</v-icon>
-              </v-btn>
-              <span>Restore</span>
-            </v-tooltip>
-            <v-tooltip left>
-              <v-btn slot="activator" icon class="mx-0" style="float:right" @click.stop="imageSnapshot(props.item)">
-                <v-icon color="green">save</v-icon>
-              </v-btn>
-              <span>Image</span>
-            </v-tooltip>
+              <v-list>
+                <v-list-tile v-for="item in snapshotActions" :key="item.title" @click="actionSnapshot(item.action, props.item)">
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
           </td>
         </tr>
       </template>
@@ -53,6 +59,7 @@
     },
     data: () => ({
       // global error
+      valid:true,
       error: '',
       container: {},
       dialog: false,
@@ -63,7 +70,17 @@
         { text: 'Actions', value: 'name', sortable: false, align: 'right' }
       ],
       //
-      items: []
+      items: [],
+      snapshotActions: [
+        { title: 'Image',  action: 'image', msg: 'Imaging', state: '' },
+        { title: 'Restore',  action: 'restore', msg: 'Restoring', state: '' },
+        { title: 'Delete',  action: 'delete', msg: 'Deleting', state: '' }
+      ],
+      nameRule: [
+        v => !!v || 'Name is required.',
+        //v => (v && /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(v)) || 'Only letters, digits or hyphens. No leading hyphen or digit. Dots are converted to hyphens.',
+        //v => (v && isNaN(v.charAt(0))) || 'Only letters, digits or hyphens. No leading hyphen or digit. Dots are converted to hyphens.'
+      ]
     }),
     beforeDestroy: function() {},
     mounted: async function () {
@@ -101,10 +118,47 @@
           //
           const response = await axios.get(this.loggedUser.sub + '/api/lxd/containers/' + this.container.info.name + '/snapshots')
           this.items = response.data.data
+          
+          this.items.forEach((item, key) => {
+            this.$set(this.items[key].config, 'user.snapshot_description', (
+              !this.items[key].config['user.snapshot_description'] || this.items[key].config['user.snapshot_description'] === '' ? '-' : this.items[key].config['user.snapshot_description'].trim()
+            ))
+            this.$set(this.items[key], 'oldNameNoContainer', item.name.substr(item.name.lastIndexOf('/') + 1))
+            this.$set(this.items[key], 'oldName', item.name)
+          })
+          
         } catch (error) {
           this.error = 'Could not fetch data from server.';
         }
         this.tableLoading = false
+      },
+      
+      actionSnapshot(action, item) {
+        //
+        if (action === 'image') {
+          this.imageSnapshot(item)
+          return
+        }
+        //
+        if (action === 'restore') {
+          this.restoreSnapshot(item)
+          return
+        }
+        //
+        if (action === 'delete') {
+          this.deleteSnapshot(item)
+          return
+        }
+        //
+        if (action === 'rename') {
+          this.renameSnapshot(item)
+          return
+        }
+        //
+        if (action === 'updateDescription') {
+          this.updateSnapshotDescription(item)
+          return
+        }
       },
       
       deleteSnapshot (item) {
@@ -218,7 +272,38 @@
         } catch (error) {
           this.$emit('snackbar', 'Failed to imaging snapshot.')
         }
+      },   
+      
+      async renameSnapshot (item) {
+        if (this.$refs.form.validate() && item.oldName !== this.container.info.name+'/'+item.oldNameNoContainer) {
+          //
+          try {
+            if (!this.loggedUser) {
+              this.$router.replace('/servers')
+            }
+  
+            //
+            const response = await axios.post(this.loggedUser.sub + '/api/lxd/containers/' + this.container.info.name + '/snapshots/' + item.oldName.substr(item.oldName.lastIndexOf('/') + 1), {
+              name: item.oldNameNoContainer,
+            })
+            
+            item.name = this.container.info.name+'/'+item.oldNameNoContainer
+            item.oldName = this.container.info.name+'/'+item.oldNameNoContainer
+            
+            this.$emit('snackbar', 'Snapshot renamed.')
+  
+          } catch (error) {
+            this.$emit('snackbar', 'Failed to rename snapshot.')
+          }
+        } else {
+          item.oldNameNoContainer = item.oldName.substr(item.oldName.lastIndexOf('/') + 1)
+        }
+      },   
+
+      safe_name(name) {
+        return name.replace(".", "-");
       }
+
     }
   }
 </script>
