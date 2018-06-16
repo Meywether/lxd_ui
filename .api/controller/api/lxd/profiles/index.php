2a3ec8097c5285f1e385f38e5291f1e8f38c722c
Lawrence Cherone <lawrence@cherone.co.uk>
2018-06-16 09:07:13 +0000

    .

--- .api/controller/api/lxd/profiles/index.php
+++ .api/controller/api/lxd/profiles/index.php
@@ -3,7 +3,7 @@
  +----------------------------------------------------------------------+
  | Conext LXD Control Panel
  +----------------------------------------------------------------------+
- | Copyright (c)2018 (https://github.com/lcherone/lxc-query)
+ | Copyright (c)2018 (https://github.com/lcherone/conext)
  +----------------------------------------------------------------------+
  | This source file is subject to MIT License
  | that is bundled with this package in the file LICENSE.
@@ -79,10 +79,9 @@ class Index extends \Base\Controller
     /**
      * GET /api/lxd/profiles
      *
-     * @param object $f3
      * @return void
      */
-    public function get(\Base $f3)
+    public function get()
     {
         try {
             //
@@ -115,7 +114,7 @@ class Index extends \Base\Controller
      * @param object $f3
      * @return void
      */
-    public function post(\Base $f3)
+    public function post()
     {
         try {
             if (empty($this->body['name'])) {
