680ca7980e3b193e540158c1ba648194680a2e0a
Lawrence Cherone <lawrence@cherone.co.uk>
2018-06-18 10:37:30 +0000

    .

--- .api/controller/api/lxd/images/index.php
+++ .api/controller/api/lxd/images/index.php
@@ -1,5 +1,22 @@
 <?php
-
+/*
+ +----------------------------------------------------------------------+
+ | Conext LXD Control Panel
+ +----------------------------------------------------------------------+
+ | Copyright (c)2018 (https://github.com/lcherone/conext)
+ +----------------------------------------------------------------------+
+ | This source file is subject to MIT License
+ | that is bundled with this package in the file LICENSE.
+ |
+ | If you did not receive a copy of the license and are unable to
+ | obtain it through the world-wide-web, please send an email
+ | to lawrence@cherone.co.uk so we can send you a copy immediately.
+ +----------------------------------------------------------------------+
+ | Authors:
+ |   Lawrence Cherone <lawrence@cherone.co.uk>
+ +----------------------------------------------------------------------+
+ */
+ 
 namespace Controller\Api\Lxd\Images;
 
 /**
@@ -10,22 +27,41 @@ class Index extends \Base\Controller
     /*
      * @var
      */
-    private $cache;
-    
-    /*
-     * @var
-     */
-    private $cache_ttl;
-    
-    /*
-     * @var
-     */
     private $lxd;
+
+    /*
+     * @var
+     */
+    protected $body = [];
     
+    /*
+     * @var
+     */
+    protected $result = []; 
     
+    /*
+     * @var
+     */
+    protected $errors = [];    
+    
+    /*
+     * @var
+     */
+    protected $cache;     
+    
+    /*
+     * @var
+     */
+    protected $cache_ttl = 3600; 
+
+    /**
+     * @param object $f3
+     * @return void
+     */
     public function beforeRoute(\Base $f3)
     {
-        // check auth
+        parent::beforeRoute($f3);
+        
         try {
             \Lib\JWT::checkAuth();
         } catch (\Exception $e) {
@@ -35,7 +71,7 @@ class Index extends \Base\Controller
                 'data'  => []
             ]);
         }
-        
+
         // check feature is enabled
         if (!in_array('images', $f3->get('modules.lxd'))) {
             $f3->status(404);
@@ -45,191 +81,75 @@ class Index extends \Base\Controller
                 'data'  => []
             ]);
         }
-        
+
+        // define model/s
         $this->lxd = new \Model\LXD($f3);
         
         $this->cache = \Cache::instance();
-        $this->cache_ttl = 5;
     }
 
     /**
+     * GET /api/lxd/images
      *
+     * @param object $f3
+     * @return void
      */
-    public function index(\Base $f3)
+    public function get(\Base $f3)
     {
-        // GET | POST | PUT | DELETE
-        $verb = $f3->get('VERB');
-
-        /**
-         * GET /api/lxd/images?remote=local
-         */
-        if ($verb === 'GET') {
+        try {
             // expect ?remote=local
             if ($f3->devoid('GET.remote')) {
-                $f3->response->json([
+                $this->result = [
                     'error' => 'Missing remote parameter',
                     'code'  => 400,
                     'data'  => []
-                ]);
-            }
-
-            try {
-                // cache remote images if not local
-                if ($f3->get('GET.remote') === 'local' || !$this->cache->exists('images.'.$f3->get('GET.remote'), $result)) {
-                    // get images filter by architecture (may add as a parameter if ever needed)
-                    $result = $this->lxd->images->list($f3->get('GET.remote'), 'architecture="'.implode('|', ['x86_64', 'i686', 'amd64']).'"');
-                    //
-                    $this->cache->set('images.'.$f3->get('GET.remote'), $result, 3600);
-                }
-                
-                $result = [
-                    'error' => null,
-                    'code'  => 200,
-                    'data'  => $result
                 ];
-            } catch (\Exception $e) {
-                $result = [
-                    'error' => $e->getMessage(),
-                    'code'  => $e->getCode(),
-                    'data'  => []
-                ];
+                return;
             }
-            $f3->response->json($result);
-        }
-        
-        /**
-         * POST /api/lxd/images
-         */
-        if ($verb === 'POST') {
-            $options = json_decode($f3->get('BODY'), true);
             
-            try {
-                $response = [
-                    'error' => null,
-                    'code'  => 200,
-                    'data'  => $this->lxd->images->create('local', $options)
-                ];
-            } catch (\Exception $e) {
-                $response = [
-                    'error' => $e->getMessage(),
-                    'code'  => $e->getCode(),
-                    'data'  => []
-                ];
+            // cache remote images if not local
+            if ($f3->get('GET.remote') === 'local' || !$this->cache->exists('images.'.$f3->get('GET.remote'), $this->result)) {
+                // get images filter by architecture (may add as a parameter if ever needed)
+                $this->result = $this->lxd->images->list($f3->get('GET.remote'), 'architecture="'.implode('|', ['x86_64', 'i686', 'amd64']).'"');
+                //
+                $this->cache->set('images.'.$f3->get('GET.remote'), $this->result, $this->cache_ttl);
             }
 
-            $f3->response->json($response);
+            $this->result = [
+                'error' => '',
+                'code'  => 200,
+                'data'  => $this->result
+            ];
+        } catch (\Exception $e) {
+            $this->result = [
+                'error' => $e->getMessage(),
+                'code'  => 422,
+                'data'  => []
+            ];
         }
     }
     
     /**
-     *
+     * POST /api/lxd/images
+     * 
+     * @return void
      */
-    public function item(\Base $f3, $params)
+    public function post()
     {
-        // GET | POST | PUT | DELETE
-        $verb = $f3->get('VERB');
-
-        /**
-         * GET /api/lxd/images/@fingerprint
-         */
-        if ($verb === 'GET') {
-            /*
-            // expect ?remote=local
-            if ($f3->devoid('GET.remote')) {
-                $f3->response->json([
-                    'error' => 'Missing remote parameter',
-                    'code'  => 400,
-                    'data'  => []
-                ]);
-            }
-            
-            // cache remote images if not local
-            if ($f3->get('GET.remote') === 'local' || !$this->cache->exists('images.'.$f3->get('GET.remote'), $result)) {
-                // get images filter by architecture (may add as a parameter if ever needed)
-                $result = $this->lxd->images->list($f3->get('GET.remote'), 'architecture="'.implode('|', ['x86_64', 'i686', 'amd64']).'"');
-                //
-                $this->cache->set('images.'.$f3->get('GET.remote'), $result, 3600);
-            }
-
-            $f3->response->json([
-                'error' => null,
-                'code'  => 200,
-                'data'  => $result
-            ]);
-            */
-        }
-        
-        /**
-         * POST /api/lxd/images/@fingerprint
-         */
-        if ($verb === 'POST') {
-            /*
-            $f3->response->json([
+        try {
+            $this->result = [
                 'error' => '',
                 'code'  => 200,
+                'data'  => $this->lxd->images->create('local', $this->body)
+            ];
+            
+            $this->cache->clear('images.local');
+        } catch (\Exception $e) {
+            $this->result = [
+                'error' => $e->getMessage(),
+                'code'  => 422,
                 'data'  => []
-            ]);
-            */
-        }
-        
-        /**
-         * PUT /api/lxd/images/@fingerprint
-         */
-        if ($verb === 'PUT') {
-            $options = json_decode($f3->get('BODY'), true);
-            
-            try {
-                $response = [
-                    'error' => null,
-                    'code'  => 200,
-                    'data'  => $this->lxd->images->replace($f3->get('GET.remote'), $params['fingerprint'], $options)
-                ];
-                
-                $this->cache->clear('images.'.$f3->get('GET.remote'));
-            } catch (\Exception $e) {
-                $response = [
-                    'error' => $e->getMessage(),
-                    'code'  => $e->getCode(),
-                    'data'  => []
-                ];
-            }
-
-            $f3->response->json($response);
-        }
-        
-        /**
-         * DELETE /api/lxd/images/@fingerprint?remote=local
-         */
-        if ($verb === 'DELETE') {
-            // expect ?remote=local
-            if ($f3->devoid('GET.remote')) {
-                $f3->response->json([
-                    'error' => 'Missing remote parameter',
-                    'code'  => 400,
-                    'data'  => []
-                ]);
-            }
-            
-            //
-            try {
-                $result = $this->lxd->images->delete($f3->get('GET.remote'), $params['fingerprint']);
-
-                $response = [
-                    'error' => null,
-                    'code'  => 200,
-                    'data'  => $result
-                ];
-                
-                $this->cache->clear('images.'.$f3->get('GET.remote'));
-            } catch (\Exception $e) {
-                $response = [
-                    'error' => $e->getMessage(),
-                    'code'  => $e->getCode(),
-                    'data'  => []
-                ];
-            }
-
-            $f3->response->json($response);
+            ];
         }
     }
 
--- .api/controller/api/lxd/images/item.php
+++ .api/controller/api/lxd/images/item.php
@@ -22,7 +22,7 @@ namespace Controller\Api\Lxd\Images;
 /**
  *
  */
-class Copy extends \Base\Controller
+class Item extends \Base\Controller
 {
     /*
      * @var
@@ -52,7 +52,7 @@ class Copy extends \Base\Controller
     /*
      * @var
      */
-    protected $cache_ttl = 3600;
+    protected $cache_ttl = 5;
     
     /**
      * @param object $f3
@@ -64,20 +64,13 @@ class Copy extends \Base\Controller
         
         try {
             \Lib\JWT::checkAuth();
+            if (!in_array('containers', $f3->get('modules.lxd'))) {
+                throw new \Exception('Feature not enabled', 404);
+            }
         } catch (\Exception $e) {
             $f3->response->json([
                 'error' => $e->getMessage(),
                 'code'  => $e->getCode(),
-                'data'  => []
-            ]);
-        }
-
-        // check feature is enabled
-        if (!in_array('containers', $f3->get('modules.lxd'))) {
-            $f3->status(404);
-            $f3->response->json([
-                'error' => 'Feature not enabled',
-                'code'  => 404,
                 'data'  => []
             ]);
         }
@@ -89,12 +82,12 @@ class Copy extends \Base\Controller
     }
 
     /**
-     * POST /api/lxd/images/@fingerprint/copy
+     * PUT /api/lxd/images/@fingerprint
      *
      * @param object $f3
      * @return void
      */
-    public function post(\Base $f3)
+    public function put(\Base $f3)
     {
         try {
             // expect ?remote=local
@@ -105,22 +98,19 @@ class Copy extends \Base\Controller
                     'data'  => []
                 ];
             }
-
-            ignore_user_abort(true);
-            set_time_limit(0);
-
-            $from_remote = escapeshellarg($f3->get('GET.remote'));
-            $to_remote = escapeshellarg($this->body['remote']);
-            $fingerprint = escapeshellarg($f3->get('PARAMS.fingerprint'));
-
+            
+            $this->body['auto_update'] = $this->body['auto_update'] == '1';
+            $this->body['cached'] = $this->body['cached'] == '1';
+            $this->body['public'] = $this->body['public'] == '1';
+            $this->body['size'] = (int) $this->body['size'];
+            
             $this->result = [
                 'error' => '',
                 'code'  => 200,
-                'data'  => $this->lxd->local('lxc image copy '.$from_remote.':'.$fingerprint.' '.$to_remote.':')
+                'data'  => $this->lxd->images->replace($f3->get('GET.remote'), $f3->get('PARAMS.fingerprint'), $this->body)
             ];
             
             $this->cache->clear('images.'.$f3->get('GET.remote'));
-            $this->cache->clear('images.'.$this->body['remote']);
         } catch (\Exception $e) {
             $this->result = [
                 'error' => $e->getMessage(),
@@ -130,4 +120,38 @@ class Copy extends \Base\Controller
         }
     }
     
+    /**
+     * DELETE /api/lxd/images/@fingerprint
+     *
+     * @param object $f3
+     * @return void
+     */
+    public function delete(\Base $f3)
+    {
+        try {
+            // expect ?remote=local
+            if ($f3->devoid('GET.remote')) {
+                $this->result = [
+                    'error' => 'Missing remote parameter',
+                    'code'  => 400,
+                    'data'  => []
+                ];
+            }
+            
+            $this->result = [
+                'error' => '',
+                'code'  => 200,
+                'data'  => $this->lxd->images->delete($f3->get('GET.remote'), $f3->get('PARAMS.fingerprint'))
+            ];
+            
+            $this->cache->clear('images.'.$f3->get('GET.remote'));
+        } catch (\Exception $e) {
+            $this->result = [
+                'error' => $e->getMessage(),
+                'code'  => 422,
+                'data'  => []
+            ];
+        }
+    }
+
 }
